01080917a34f3fc0aed25cb214e59e47dc884e62
yobmod <yobmod@gmail.com>
2020-02-27 10:47:20 +0000

    add poetry lock

--- src/parser.py
+++ src/parser.py
@@ -14,26 +14,71 @@ FormulaType = str
 
 colorama.init()
 
-ATOM_REGEX = r'([A-Z][a-z]*)(\d*)'
-OPENERS = r'({['
-CLOSERS = r')}]'
+ATOM_REGEX = r"([A-Z][a-z]*)(\d*)"
+OPENERS = r"({["
+CLOSERS = r")}]"
 
 
 def is_balanced(formula: FormulaType) -> bool:
     """Check if all sort of brackets come in pairs."""
     # Very naive check, just here because you always need some input checking
     c = Counter(formula)
-    return c['['] == c[']'] and c['{'] == c['}'] and c['('] == c[')']
+    return c["["] == c["]"] and c["{"] == c["}"] and c["("] == c[")"]
 
 
 def is_real_elements(formula: FormulaType) -> bool:
     """Some basic string chacks to be sure there are some elements present"""
 
-    elem_first_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    elem_sec_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'r', 's', 't', 'u', 'v', 'y']
+    elem_first_letters = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    elem_sec_letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "l",
+        "m",
+        "n",
+        "o",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "y",
+    ]
     elem_letters = elem_first_letters + elem_sec_letters
     # non_letter = ['J', 'Q', 'j', 'k', 'p', 'q', 'w', 'x', 'z']
-    allowed_symbols = ['[', ']', '(', ')', '{', '}', '-', '–']
+    allowed_symbols = ["[", "]", "(", ")", "{", "}", "-", "–"]
 
     # Check if formula string contains only elemental letters, or allowed symbols.
     for letter in formula:
@@ -50,7 +95,11 @@ def is_real_elements(formula: FormulaType) -> bool:
         return False
 
     # check first character (if not a bracket) is an element
-    if formula[0].isalpha() and not formula[0].isupper() and formula[0] not in elem_first_letters:
+    if (
+        formula[0].isalpha()
+        and not formula[0].isupper()
+        and formula[0] not in elem_first_letters
+    ):
         return False
 
     # check first character is not a number
@@ -72,12 +121,17 @@ def _dictify(tuples: Iterable[Tuple[str, str]]) -> Dict[str, int]:
     return res
 
 
-def _fuse(mol1: Mapping[str, int], mol2: Mapping[str, int], w: int = 1) -> Dict[str, int]:
+def _fuse(
+    mol1: Mapping[str, int], mol2: Mapping[str, int], w: int = 1
+) -> Dict[str, int]:
     """
     Fuse 2 dicts representing molecules. Return a new dict.
     This fusion does not follow the laws of physics.
     """
-    return {atom: (mol1.get(atom, 0) + mol2.get(atom, 0)) * w for atom in set(mol1) | set(mol2)}
+    return {
+        atom: (mol1.get(atom, 0) + mol2.get(atom, 0)) * w
+        for atom in set(mol1) | set(mol2)
+    }
 
 
 def _parse(formula: FormulaType) -> Tuple[Dict[str, int], int]:
@@ -96,18 +150,18 @@ def _parse(formula: FormulaType) -> Tuple[Dict[str, int], int]:
 
         if token in CLOSERS:
             # Check for an index for this part
-            m = re.match(r'\d+', formula[i+1:])
+            m = re.match(r"\d+", formula[i + 1 :])
             if m:
                 weight = int(m.group(0))
                 i += len(m.group(0))
             else:
                 weight = 1
 
-            submol = _dictify(re.findall(ATOM_REGEX, ''.join(q)))
+            submol = _dictify(re.findall(ATOM_REGEX, "".join(q)))
             return _fuse(mol, submol, weight), i
 
         elif token in OPENERS:
-            submol, sublength = _parse(formula[i+1:])
+            submol, sublength = _parse(formula[i + 1 :])
             mol = _fuse(mol, submol)
             # skip the already read submol
             i += sublength + 1
@@ -117,28 +171,34 @@ def _parse(formula: FormulaType) -> Tuple[Dict[str, int], int]:
         i += 1
 
     # Fuse in all that's left at base level
-    return _fuse(mol, _dictify(re.findall(ATOM_REGEX, ''.join(q)))), i
+    return _fuse(mol, _dictify(re.findall(ATOM_REGEX, "".join(q)))), i
 
 
 def parse_formula(formula: FormulaType) -> Dict[str, int]:
     """Parse the formula and return a dict with occurences of each atom."""
 
-    warn_text = (colored('Check your formula [', 'yellow', attrs=['bold']) +
-                 colored(formula, 'red', attrs=['bold']) +
-                 colored('] - are all the element symbols correct?\n', 'yellow', attrs=['bold']))
+    warn_text = (
+        colored("Check your formula [", "yellow", attrs=["bold"])
+        + colored(formula, "red", attrs=["bold"])
+        + colored(
+            "] - are all the element symbols correct?\n", "yellow", attrs=["bold"]
+        )
+    )
 
     if not is_real_elements(formula):
         warnings.warn(warn_text, category=UserWarning, stacklevel=4)
 
     try:
         if not is_balanced(formula):
-            raise ValueError(colored("Watch your brackets ![{]$[&?)]}!]", 'red', attrs=['bold']))
+            raise ValueError(
+                colored("Watch your brackets ![{]$[&?)]}!]", "red", attrs=["bold"])
+            )
     except ValueError as v_err:
         exc_type, exc_obj, exc_tb = sys.exc_info()
-        #fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        #print(exc_type, fname, exc_tb.tb_lineno)
+        # fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        # print(exc_type, fname, exc_tb.tb_lineno)
         if exc_type:
-            print(f'Error: {exc_type.__name__}: {v_err}\n')
+            print(f"Error: {exc_type.__name__}: {v_err}\n")
         sys.exit()
 
     parsed = _parse(formula)[0]
@@ -155,8 +215,8 @@ def parse_print_from_input() -> None:
     formula = str(input("\nEnter chemical formula: \n"))
     print("")
     res = parse_formula(formula)
-    colored_res = colored(str(res), 'green', attrs=['bold'])
-    print(f'Dict of elements from given formula: {colored_res}\n')
+    colored_res = colored(str(res), "green", attrs=["bold"])
+    print(f"Dict of elements from given formula: {colored_res}\n")
 
 
 if __name__ == "__main__":
--- src/urania.py
+++ src/urania.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-# from dataclasses import asdict
 from typing_extensions import Final
 from srim import Layer, Target
 from dcf import create_ion_list, pool_srim, elem_u_dict, elem_o_dict, elem_si_dict
@@ -7,32 +6,62 @@ from dcf import create_ion_list, pool_srim, elem_u_dict, elem_o_dict, elem_si_di
 if __name__ == "__main__":
 
     # set path to save data
-    data_path: Final[Path] = Path(R'.\data\uo2pure')
+    data_path: Final[Path] = Path(r".\data\uo2pure")
 
     # poin to srim exec
-    srim_executable_directory: Final[Path] = Path(R'C:\srim')
+    srim_executable_directory: Final[Path] = Path(r"C:\srim")
 
     # create list of ions from list of energies in keV
-    energy_kev_list = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
-                       1250, 1500, 1750, 2000, 2500, 3000, 4000, 5000]
-    ions_He_list = create_ion_list('He', energy_kev_list, units='kev')
+    energy_kev_list = [
+        100,
+        200,
+        300,
+        400,
+        500,
+        600,
+        700,
+        800,
+        900,
+        1000,
+        1250,
+        1500,
+        1750,
+        2000,
+        2500,
+        3000,
+        4000,
+        5000,
+    ]
+
+    ions_He_list = create_ion_list("He", energy_kev_list, units="kev")
 
     # create target from list of layers
     layer_UO2_10um = Layer(
-        {'U': {'stoich': 1.0, **elem_u_dict},
-         'O': {'stoich': 2.0, **elem_o_dict.as_dict()},
-         },
-        density=10.97, width=100_000.0, name='urania'
+        {
+            "U": {"stoich": 1.0, **elem_u_dict},
+            "O": {"stoich": 2.0, **elem_o_dict.as_dict()},
+        },
+        density=10.97,
+        width=100_000.0,
+        name="urania",
     )
 
     layer_SiO2_10um = Layer(
-        {'Si': {'stoich': 1.0, **elem_si_dict},
-         'O': {'stoich': 2.0, **elem_o_dict.as_dict()},
-         },
-        density=2.65, width=100_000.0, name='silica'
+        {
+            "Si": {"stoich": 1.0, **elem_si_dict},
+            "O": {"stoich": 2.0, **elem_o_dict.as_dict()},
+        },
+        density=2.65,
+        width=100_000.0,
+        name="silica",
     )
 
     target = Target([layer_UO2_10um])
 
-    data_list = pool_srim(ions_He_list, target, data_path,
-                          num_ions=1000_000, srim_dir=srim_executable_directory)
+    data_list = pool_srim(
+        ions_He_list,
+        target,
+        data_path,
+        num_ions=1000_000,
+        srim_dir=srim_executable_directory,
+    )
